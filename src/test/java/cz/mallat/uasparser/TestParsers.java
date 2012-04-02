e529cd3fc56b3c9c5e102395462a2f363140d43f
Chetan Sarva <chetan@betteradvertising.com>
2012-04-02 16:47:35 -0400

    updated basic unit tests

--- src/test/java/cz/mallat/uasparser/TestParsers.java
+++ src/test/java/cz/mallat/uasparser/TestParsers.java
@@ -1,59 +1,65 @@
 package cz.mallat.uasparser;
 
+import static org.junit.Assert.*;
+
 import java.io.IOException;
 
 import org.junit.Test;
 
-import cz.mallat.uasparser.CachingOnlineUpdateUASparser;
-import cz.mallat.uasparser.OnlineUpdateUASparser;
-import cz.mallat.uasparser.UASparser;
-import cz.mallat.uasparser.UserAgentInfo;
-
 /**
  * No real JUnit tests
- * 
+ *
  * @author oli
  *
  */
 public class TestParsers {
 
-	@Test
-	public void runUAParser() throws IOException {
-		UASparser p = new UASparser(getClass().getClassLoader().getResourceAsStream("uas.ini"));
-		UserAgentInfo uai = p.parse("Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.12) Gecko/2009070611 Firefox/3.0.12");
-		System.out.println("type:" + uai.getTyp());
-		System.out.println("ua_name:" + uai.getUaName());
-		System.out.println("ua_family:" + uai.getUaFamily());
-		System.out.println("ua_producer:" + uai.getUaCompany());
-		System.out.println("os_name:" + uai.getOsName());
-		System.out.println("os_family:" + uai.getOsFamily());
-		System.out.println("os_producer:" + uai.getOsCompany());
-	}
+    @Test
+    public void runUAParser() throws IOException {
+        UASparser p = new UASparser(getClass().getClassLoader().getResourceAsStream("uas.ini"));
+        UserAgentInfo uai = p.parse("Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.12) Gecko/2009070611 Firefox/3.0.12");
 
-	@Test
-	public void runOnlineUAParser() throws IOException {
-		UASparser p = new OnlineUpdateUASparser();
-		UserAgentInfo uai = p.parse("Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.12) Gecko/2009070611 Firefox/3.0.12");
-		System.out.println("type:" + uai.getTyp());
-		System.out.println("ua_name:" + uai.getUaName());
-		System.out.println("ua_family:" + uai.getUaFamily());
-		System.out.println("ua_producer:" + uai.getUaCompany());
-		System.out.println("os_name:" + uai.getOsName());
-		System.out.println("os_family:" + uai.getOsFamily());
-		System.out.println("os_producer:" + uai.getOsCompany());
-	}
+        // type:Browser
+        // ua_name:Firefox 3.0.12
+        // ua_family:Firefox
+        // ua_producer:Mozilla Foundation
+        // os_name:Windows XP
+        // os_family:Windows
+        // os_producer:Microsoft Corporation.
 
-	@Test
-	public void runCachedOnlineUAParser() throws IOException {
-		UASparser p = new CachingOnlineUpdateUASparser();
-		UserAgentInfo uai = p.parse("Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.12) Gecko/2009070611 Firefox/3.0.12");
-		System.out.println("type:" + uai.getTyp());
-		System.out.println("ua_name:" + uai.getUaName());
-		System.out.println("ua_family:" + uai.getUaFamily());
-		System.out.println("ua_producer:" + uai.getUaCompany());
-		System.out.println("os_name:" + uai.getOsName());
-		System.out.println("os_family:" + uai.getOsFamily());
-		System.out.println("os_producer:" + uai.getOsCompany());
-	}
+        assertEquals("Browser", uai.getTyp());
+        assertEquals("Firefox 3.0.12", uai.getUaName());
+        assertEquals("Firefox", uai.getUaFamily());
+        assertEquals("Mozilla Foundation", uai.getUaCompany());
+        assertEquals("Windows XP", uai.getOsName());
+        assertEquals("Windows", uai.getOsFamily());
+        assertEquals("Microsoft Corporation.", uai.getOsCompany());
+    }
+
+    @Test
+    public void runOnlineUAParser() throws IOException {
+        UASparser p = new OnlineUpdateUASparser();
+        UserAgentInfo uai = p.parse("Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.12) Gecko/2009070611 Firefox/3.0.12");
+        assertEquals("Browser", uai.getTyp());
+        assertEquals("Firefox 3.0.12", uai.getUaName());
+        assertEquals("Firefox", uai.getUaFamily());
+        assertEquals("Mozilla Foundation", uai.getUaCompany());
+        assertEquals("Windows XP", uai.getOsName());
+        assertEquals("Windows", uai.getOsFamily());
+        assertEquals("Microsoft Corporation.", uai.getOsCompany());
+    }
+
+    @Test
+    public void runCachedOnlineUAParser() throws IOException {
+        UASparser p = new CachingOnlineUpdateUASparser();
+        UserAgentInfo uai = p.parse("Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.12) Gecko/2009070611 Firefox/3.0.12");
+        assertEquals("Browser", uai.getTyp());
+        assertEquals("Firefox 3.0.12", uai.getUaName());
+        assertEquals("Firefox", uai.getUaFamily());
+        assertEquals("Mozilla Foundation", uai.getUaCompany());
+        assertEquals("Windows XP", uai.getOsName());
+        assertEquals("Windows", uai.getOsFamily());
+        assertEquals("Microsoft Corporation.", uai.getOsCompany());
+    }
 
 }
