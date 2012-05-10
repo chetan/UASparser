772fcdf94789648ef26fd572d3ad38feaa71e72e
Chetan Sarva <chetan@betteradvertising.com>
2012-05-10 16:31:37 -0400

    disabled php test

--- src/test/java/cz/mallat/uasparser/TestPHPFileParser.java
+++ src/test/java/cz/mallat/uasparser/TestPHPFileParser.java
@@ -1,33 +1,27 @@
 package cz.mallat.uasparser;
 
-import java.io.IOException;
-
-import org.junit.Test;
-
-import cz.mallat.uasparser.fileparser.Entry;
-import cz.mallat.uasparser.fileparser.PHPFileParser;
-import cz.mallat.uasparser.fileparser.Section;
 
 /**
  * No real JUnit tests
- * 
+ *
  * @author oli
- * 
+ *
  */
 public class TestPHPFileParser {
 
-	@Test
-	public void runAll() throws IOException {
-		long time = System.currentTimeMillis();
-		PHPFileParser fp = new PHPFileParser(getClass().getClassLoader().getResourceAsStream("uas.ini"));
-		System.out.println("parsing time : " + (System.currentTimeMillis() - time));
-		for (Section sec : fp.getSections()) {
-			System.out.println("[" + sec.getName() + "]");
-			for (Entry e : sec.getEntries()) {
-				for (String s : e.getData()) {
-					System.out.println(e.getKey() + "[] = \"" + s + "\"");
-				}
-			}
-		}
-	}
+//	@Test
+//	public void runAll() throws IOException {
+//		long time = System.currentTimeMillis();
+//		PHPFileParser fp = new PHPFileParser(getClass().getClassLoader().getResourceAsStream("uas.ini"));
+//		System.out.println("parsing time : " + (System.currentTimeMillis() - time));
+//		for (Section sec : fp.getSections()) {
+//			System.out.println("[" + sec.getName() + "]");
+//			for (Entry e : sec.getEntries()) {
+//				for (String s : e.getData()) {
+//					System.out.println(e.getKey() + "[] = \"" + s + "\"");
+//				}
+//			}
+//		}
+//	}
+
 }
