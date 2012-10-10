2f5c3bf7d8a4688e80528d2262e219ecd2f0cba7
Chetan Sarva <chetan@pixelcop.net>
2012-10-10 18:51:13 -0400

    testsuite

---
+++ src/test/java/cz/mallat/uasparser/TestSuite.java
@@ -0,0 +1,11 @@
+package cz.mallat.uasparser;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ TestOnlineUpdater.class, TestParsers.class })
+public class TestSuite {
+
+}
