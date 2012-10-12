5fc22de4654476eda6256ffeed090e74d3bc74c8
Chetan Sarva <chetan@pixelcop.net>
2012-10-12 11:01:57 -0400

    modified so it can be correctly updated

--- src/main/java/cz/mallat/uasparser/MultithreadedUASparser.java
+++ src/main/java/cz/mallat/uasparser/MultithreadedUASparser.java
@@ -21,30 +21,9 @@ import jregex.Pattern;
  */
 public class MultithreadedUASparser extends SingleThreadedUASparser {
 
-    private final ThreadLocal<Map<Matcher, Long>> compiledBrowserMatcherMapT = new ThreadLocal<Map<Matcher, Long>>() {
-        @Override
-        protected Map<Matcher, Long> initialValue() {
-            LinkedHashMap<Matcher, Long> tmpMap = new LinkedHashMap<Matcher, Long>(browserRegMap.size());
-            for (Map.Entry<String, Long> entry : browserRegMap.entrySet()) {
-                Pattern pattern = new Pattern(entry.getKey(), Pattern.IGNORE_CASE | Pattern.DOTALL);
-                tmpMap.put(pattern.matcher(), entry.getValue());
-            }
-            return tmpMap;
-        }
-    };
+    private ThreadLocal<Map<Matcher, Long>> compiledBrowserMatcherMapT;
 
-    private final ThreadLocal<Map<Matcher, Long>> compiledOsMatcherMapT = new ThreadLocal<Map<Matcher, Long>>() {
-        @Override
-        protected Map<Matcher, Long> initialValue() {
-            LinkedHashMap<Matcher, Long> tmpMap = new LinkedHashMap<Matcher, Long>(osRegMap.size());
-            for (Map.Entry<String, Long> entry : osRegMap.entrySet()) {
-                Pattern pattern = new Pattern(entry.getKey(), Pattern.IGNORE_CASE | Pattern.DOTALL);
-                tmpMap.put(pattern.matcher(), entry.getValue());
-            }
-
-            return tmpMap;
-        }
-    };
+    private ThreadLocal<Map<Matcher, Long>> compiledOsMatcherMapT;
 
     public MultithreadedUASparser(InputStream inputStreamToDefinitionFile) throws IOException {
         super(inputStreamToDefinitionFile);
@@ -52,6 +31,37 @@ public class MultithreadedUASparser extends SingleThreadedUASparser {
 
     public MultithreadedUASparser(String localDefinitionFilename) throws IOException {
         super(localDefinitionFilename);
+    }
+
+    @Override
+    protected void preCompileBrowserRegMap() {
+        compiledBrowserMatcherMapT = new ThreadLocal<Map<Matcher, Long>>() {
+            @Override
+            protected Map<Matcher, Long> initialValue() {
+                LinkedHashMap<Matcher, Long> tmpMap = new LinkedHashMap<Matcher, Long>(browserRegMap.size());
+                for (Map.Entry<String, Long> entry : browserRegMap.entrySet()) {
+                    Pattern pattern = new Pattern(entry.getKey(), Pattern.IGNORE_CASE | Pattern.DOTALL);
+                    tmpMap.put(pattern.matcher(), entry.getValue());
+                }
+                return tmpMap;
+            }
+        };
+    }
+
+    @Override
+    protected void preCompileOsRegMap() {
+        compiledOsMatcherMapT = new ThreadLocal<Map<Matcher, Long>>() {
+            @Override
+            protected Map<Matcher, Long> initialValue() {
+                LinkedHashMap<Matcher, Long> tmpMap = new LinkedHashMap<Matcher, Long>(osRegMap.size());
+                for (Map.Entry<String, Long> entry : osRegMap.entrySet()) {
+                    Pattern pattern = new Pattern(entry.getKey(), Pattern.IGNORE_CASE | Pattern.DOTALL);
+                    tmpMap.put(pattern.matcher(), entry.getValue());
+                }
+
+                return tmpMap;
+            }
+        };
     }
 
     @Override
