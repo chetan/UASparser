b38825d49601bcbdae5eeb1f235fecf70353325b
fsiegrist <felix.siegrist@inventage.com>
2013-10-08 11:05:09 +0200

    added support for the [device] and [device_reg] sections

---
+++ src/main/java/cz/mallat/uasparser/DeviceEntry.java
@@ -0,0 +1,49 @@
+package cz.mallat.uasparser;
+
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * Java bean that holds the data from the [device] section in the data file.
+ * 
+ * @author Felix Siegrist, Inventage AG
+ * 
+ */
+class DeviceEntry {
+
+	private String type;
+	private String ico;
+	private String infoUrl;
+
+	public DeviceEntry(List<String> data) {
+		Iterator<String> it = data.iterator();
+		this.type = it.next();
+		this.ico = it.next();
+		this.infoUrl = it.next();
+	}
+
+	public String getType() {
+		return type;
+	}
+
+	public void setType(String type) {
+		this.type = type;
+	}
+
+	public String getIco() {
+		return ico;
+	}
+
+	public void setIco(String ico) {
+		this.ico = ico;
+	}
+
+	public String getInfoUrl() {
+		return infoUrl;
+	}
+
+	public void setInfoUrl(String infoUrl) {
+		this.infoUrl = infoUrl;
+	}
+
+}
